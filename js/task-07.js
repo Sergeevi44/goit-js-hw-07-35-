293ebccfb00102d185dda29f6251aa2b2d1d3d92
Oleksii Borshch <Sergeevi44@gmail.com>
2021-07-19 05:58:16 +0300

    7
    done

---
+++ js/task-07.js
@@ -0,0 +1,12 @@
+const refs = {
+	rangeRef: document.querySelector('#font-size-control'),
+	textRef:document.querySelector('#text')
+}
+console.log(refs.rangeRef);
+console.log(refs.textRef);
+const changeTextSize = refs.rangeRef.addEventListener('input', onChangeValueRange);
+function onChangeValueRange(event) {
+	const newFontSize = Number(event.currentTarget.value);
+	// refs.textRef.setAttribute('style', `font-size:${newFontSize}px`);
+	refs.textRef.style.fontSize = `${newFontSize}px`;
+}
