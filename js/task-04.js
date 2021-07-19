dfa9732137bbeccafb5e6a9c60d4ac056c41cc92
Oleksii Borshch <Sergeevi44@gmail.com>
2021-07-19 03:25:03 +0300

    4
    fix

--- js/task-04.js
+++ js/task-04.js
@@ -1,7 +1,7 @@
 let counterValue = Number(document.querySelector('#value').textContent);
 console.log(counterValue);
 console.log(typeof (counterValue));
-let counterValueRef = document.querySelector('#value');
+const counterValueRef = document.querySelector('#value');
 const decrementBtnRef = document.querySelector('#counter button[data-action="decrement"]');
 console.log(decrementBtnRef);
 const incrementBtnRef = document.querySelector('#counter button[data-action="increment"]');
