f07a48b2bec7bc9ba96528801c27f90da73923f1
Oleksii Borshch <Sergeevi44@gmail.com>
2021-07-20 11:35:45 +0300

    6
    fix bug

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,8 @@
 const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', onInputLengthChecker);
 function onInputLengthChecker(event) {
+	inputEl.classList.add('valid');
 	event.currentTarget.value.length !== Number(inputEl.getAttribute('data-length')) ?
 		inputEl.classList.add('invalid') :
-		inputEl.classList.replace('invalid','valid');
+		inputEl.classList.remove('invalid');
 }
